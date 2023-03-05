720eb20d2a0682bafe55ccbc41f3411f4f0cdef1
mrtstg <russiankurwacsgo@gmail.com>
2023-03-05 17:00:44 +0300

    added hosts read

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -2,11 +2,24 @@ import csv
 import os
 import contextlib
 from typing import IO, Generator
-from validators import validate_row
+from validators import validate_address, validate_row
+import enum
+from dataclasses import dataclass
 
 
 class FileNotFound(Exception):
     pass
+
+
+class HostType(enum.Enum):
+    DOMAIN, IP = range(2)
+
+
+@dataclass
+class Host:
+    type: int  # but actually HostType member
+    address: str
+    ports: list[int]
 
 
 class CsvFileReader:
@@ -18,19 +31,20 @@ class CsvFileReader:
         yield file
         file.close()
 
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, dns_adapter):
         if not os.path.exists(file_path):
             raise FileNotFound("Файл не найден!")
         self.file_path = file_path
+        self.dns_adapter = dns_adapter
 
-    def read_raw_data(self, dns_adapter) -> list[tuple[str, list[int]]]:
+    def read_raw_data(self) -> list[tuple[str, list[int]]]:
         raw_data = []
         with self.open_file() as file:
             csv_reader = csv.reader(file, delimiter=";")
             for row_index, row in enumerate(csv_reader, 1):
                 if row_index == 1:
                     continue
-                validate_row(row, dns_adapter)
+                validate_row(row, self.dns_adapter)
                 processed_ports: list[int] = list(
                     map(
                         lambda x: int(x.strip()),
@@ -39,3 +53,10 @@ class CsvFileReader:
                 )
                 raw_data.append([row[0], processed_ports])
         return raw_data
+
+    def read_hosts_list(self) -> list[Host]:
+        hosts = []
+        for row in self.read_raw_data():
+            host_type = HostType.IP if validate_address(row[0]) else HostType.DOMAIN
+            hosts.append(Host(host_type, row[0], row[1]))
+        return hosts
