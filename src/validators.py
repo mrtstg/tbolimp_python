a3b6333bcd6ff508c213586a528ee47df45b59bb
mrtstg <russiankurwacsgo@gmail.com>
2023-03-05 13:19:58 +0300

    changed order of rows check

--- src/validators.py
+++ src/validators.py
@@ -1,4 +1,5 @@
 import ipaddress
+from dns_worker import DomainNameNotAbsolute, DomainNameNotFound
 
 
 def validate_address(address: str) -> bool:
@@ -10,11 +11,20 @@ def validate_address(address: str) -> bool:
 
 
 def validate_row(row: list[str], dns_adapter):
-    assert len(row[0].strip()) > 0, "IP-адрес или доменное имя не может быть пустым!"
-    if not dns_adapter.is_resolvable(row[0]):
-        assert validate_address(row[0]), (
-            "Некорректный IP-адрес/доменное имя! (%s)" % row[0]
-        )
+    domain_or_address: str = row[0].strip()
+    assert len(row[0]) > 0, "IP-адрес или доменное имя не может быть пустым!"
+    if not validate_address(domain_or_address):
+        try:
+            dns_adapter.resolve_ipv4(domain_or_address)
+            dns_adapter.resolve_ipv6(domain_or_address)
+        except (DomainNameNotAbsolute, DomainNameNotFound):
+            assert validate_address(row[0]), (
+                "Некорректный IP-адрес/доменное имя %s. Если был введен домен, проверьте "
+                "существует ли он. Если был введен IP-адрес, проверьте его коррекность"
+                % row[0]
+            )
+        except Exception:
+            pass
 
     ports: list[str] = list(
         filter(lambda x: len(x) > 0, map(lambda x: str(x).strip(), row[1].split(",")))
